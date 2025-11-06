96a3dd0b77c692232968d1967e73a965661dee93
Pham Duc Long <mrlong01012020@gmail.com>
2025-11-06 21:21:29 +0700

    04: Database setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/protected(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isProtectedRoute(req)) {
+  if (isProtectedRoute(req)) {
     await auth.protect();
   }
 });
