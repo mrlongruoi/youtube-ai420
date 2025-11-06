21cec07acac6eaa11683e8cf593d416a7152e553
Pham Duc Long <mrlong01012020@gmail.com>
2025-11-06 20:35:56 +0700

    03: Authentication

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,14 +1,33 @@
+"use client";
+
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/nextjs";
 import { UserCircleIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
 export const AuthButton = () => {
     return (
-        <Button
-            variant="outline"
-            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none [&_svg]:size-5"
-        >
-            <UserCircleIcon />
-            Sign In
-        </Button>
+        <>
+            <SignedIn>
+                <UserButton />
+                {/* add menu items for studio and user profile */}
+            </SignedIn>
+            
+            <SignedOut>
+                <SignInButton mode="modal">
+                    <Button
+                        variant="outline"
+                        className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none [&_svg]:size-5"
+                    >
+                        <UserCircleIcon />
+                        Sign In
+                    </Button>
+                </SignInButton>
+            </SignedOut>
+        </>
     )
 }
