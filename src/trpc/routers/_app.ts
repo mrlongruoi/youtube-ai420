6cf20fa42c23bc556d7587eef085459fc9fd287a
Pham Duc Long <mrlong01012020@gmail.com>
2025-11-07 12:32:25 +0700

    07: TRPC configuration

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,14 +1,16 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from "zod";
+import { protectedProcedure, createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
-      }),
+      })
     )
     .query((opts) => {
+      console.log({ dbUser: opts.ctx.user });
+
       return {
         greeting: `hello ${opts.input.text}`,
       };
